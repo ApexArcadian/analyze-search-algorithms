78bc155f692f5ea21b8b343d1d532ccb4252c911
ApexArcadian <85552345+ApexArcadian@users.noreply.github.com>
2026-03-22 20:44:43 -0400

    branch main created

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ target1 = 12  # Present
 target2 = 9   # Not present
 
 index0 = recursive_binary_search(arr, target1, 0, len(arr) - 1)
-print(f"{target1} found at index: {index}" if index != -1 else f"{target1} not found")
+print(f"{target1} found at index: {index0}" if index0 != -1 else f"{target1} not found")
 
 index1 = iterative_binary_search(arr, target1)
-print(f"{target1} found at index: {index}" if index != -1 else f"{target1} not found")
+print(f"{target1} found at index: {index1}" if index1 != -1 else f"{target1} not found")
       
 index2 = sequential_search(arr, target1)
-print(f"{target1} found at index: {index}" if index != -1 else f"{target1} not found")
+print(f"{target1} found at index: {index2}" if index2 != -1 else f"{target1} not found")
