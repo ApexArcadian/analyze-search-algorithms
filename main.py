d99586f83479f24fc2fef96a96c753d97138cd15
ApexArcadian <85552345+ApexArcadian@users.noreply.github.com>
2026-03-25 21:37:59 -0400

    change to sorted list and fix formatting

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from search_algorithms import recursive_binary_search, iterative_binary_search, sequential_search
 import random
 
-arr = [random.randint(1, 100) for _ in range(20)]
-sorted = arr.sort()
+arr = sorted([random.randint(1, 1000000) for _ in range(20)])
+
 target = random.choice(arr) if random.random() < 0.5 else 999 # 50% of failure
+
 index = recursive_binary_search(arr, target, 0, len(arr) - 1)
+index1 = iterative_binary_search(arr, target)
+index2 = sequential_search(arr, target)
+
 print(f"{target} found at index: {index}" if index != -1 else f"{target} not found")
 
-index1 = iterative_binary_search(arr, target)
+
 print(f"{target} found at index: {index}" if index1 != -1 else f"{target} not found")
       
-index2 = sequential_search(arr, target)
+
 print(f"{target} found at index: {index}" if index2 != -1 else f"{target} not found")
