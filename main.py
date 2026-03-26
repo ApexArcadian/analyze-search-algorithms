c8dda5a1980a616db00c02eae09e3f99bf741386
ApexArcadian <85552345+ApexArcadian@users.noreply.github.com>
2026-03-25 21:05:57 -0400

    creating random numbers branch
    added random generated list

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from search_algorithms import recursive_binary_search, iterative_binary_search, sequential_search
-# import search_algorithms and create test list
-arr = [3, 5, 8, 12, 14, 18, 21]
-arr.sort()
-target1 = 12  # Present
-target2 = 9   # Not present
+import random
 
+arr = [random.randint(1, 100) for _ in range(20)]
+arr = arr.sort()
+target = random.choice(arr) if random.random() < 0.5 else 999 # 50% of failure
 index0 = recursive_binary_search(arr, target1, 0, len(arr) - 1)
 print(f"{target1} found at index: {index0}" if index0 != -1 else f"{target1} not found")
 
