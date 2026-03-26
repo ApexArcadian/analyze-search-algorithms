8612419d285498c9c9e8b8e27b34444c61d0d1ec
ApexArcadian <85552345+ApexArcadian@users.noreply.github.com>
2026-03-25 21:33:40 -0400

    fixing basic syntactic errors

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from search_algorithms import recursive_binary_search, iterative_binary_search, 
 import random
 
 arr = [random.randint(1, 100) for _ in range(20)]
-arr = arr.sort()
+sorted = arr.sort()
 target = random.choice(arr) if random.random() < 0.5 else 999 # 50% of failure
-index0 = recursive_binary_search(arr, target1, 0, len(arr) - 1)
-print(f"{target1} found at index: {index0}" if index0 != -1 else f"{target1} not found")
+index = recursive_binary_search(arr, target, 0, len(arr) - 1)
+print(f"{target} found at index: {index}" if index != -1 else f"{target} not found")
 
-index1 = iterative_binary_search(arr, target1)
-print(f"{target1} found at index: {index1}" if index1 != -1 else f"{target1} not found")
+index1 = iterative_binary_search(arr, target)
+print(f"{target} found at index: {index}" if index1 != -1 else f"{target} not found")
       
-index2 = sequential_search(arr, target1)
-print(f"{target1} found at index: {index2}" if index2 != -1 else f"{target1} not found")
+index2 = sequential_search(arr, target)
+print(f"{target} found at index: {index}" if index2 != -1 else f"{target} not found")
